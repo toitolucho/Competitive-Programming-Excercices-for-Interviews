203f64265c72292e0923b62b449e49d9a539da7c
Luis Molina <lmolina@pros.com>
2023-02-09 09:03:49 -0400

    Codility exercices added for practicing

--- Codility/Program.cs
+++ Codility/Program.cs
@@ -1,20 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Codility;
 
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
 
-CyclicRotation cs = new CyclicRotation();
-var dato = cs.solution(new int [] {0,1,2}, 22);
-// var dato = cs.solution(new int [] {0,0,0}, 1);
-//var dato = cs.solution(new int [] {}, 0);
-printArray(dato);
+// OddOccurrencesInArray oca = new OddOccurrencesInArray();
+// int resp = oca.solution(new int [] {1000000, 2000000, 1000000, 30000000, 5000000, 2000000, 30000000});
+// Console.WriteLine("Number is " + resp);
+
+// FrogJmp frog = new FrogJmp();
+// Console.WriteLine( frog.solution(10,11,5));
+
+
+PermMissingElem frog = new PermMissingElem();
+Console.WriteLine( frog.solution(new int[] {1,2,3}));
 
 
 
- static void printArray(int[] dato)
-{
-    dato.ToList().ForEach(a =>
-        Console.Write("{0}, ", a)
-    );
-    Console.WriteLine();
-}
+// CyclicRotation cs = new CyclicRotation();
+// var dato = cs.solution(new int [] {0,1,2}, 22);
+// // var dato = cs.solution(new int [] {0,0,0}, 1);
+// //var dato = cs.solution(new int [] {}, 0);
+// printArray(dato);
+
+
+
+//  static void printArray(int[] dato)
+// {
+//     dato.ToList().ForEach(a =>
+//         Console.Write("{0}, ", a)
+//     );
+//     Console.WriteLine();
+// }
